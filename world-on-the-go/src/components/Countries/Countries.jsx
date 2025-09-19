2da384c75227a63bffca8fa1add2ca8e5fe7624c
joybappy2 <fan328547@gmail.com>
2025-09-19 23:19:25 +0600

    error fixed

--- world-on-the-go/src/components/Countries/Countries.jsx
+++ world-on-the-go/src/components/Countries/Countries.jsx
@@ -1,12 +1,23 @@
 import { use } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
+
   return (
     <>
-      <Country countries={countries}></Country>
+      <h1 className="title">Countries</h1>
+      <div className="country-container">
+        {countries.map((country) => {
+        return (
+          <div key={country.ccn3.ccn3}>
+            <Country key={country.ccn3.ccn3} country={country}></Country>
+          </div>
+        );
+      })}
+      </div>
     </>
   );
 };
--- world-on-the-go/src/components/Country/Country.jsx
+++ world-on-the-go/src/components/Country/Country.jsx
@@ -1,32 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ countries }) => {
-  const clickHandle = () => {
-    const btns = document.querySelectorAll(".vbtn");
-    btns.forEach((btn) => {});
-  };
+const Country = ( { country } ) => {
+  const [visited, setVisited] = useState(false)
+  const visitHandler = () => {
+    setVisited(!visited)
+    
+  }
   return (
     <>
-      <h1 className="title">Countries</h1>
-      <div className="country-container">
-        {countries.map((country) => {
-          return (
-            <div className="countrystyle" key={country.cca3.cca3}>
-              <img src={country.flags.flags.png} alt="" />
-              <h3>Name: {country.name.common}</h3>
-              <p>Population: {country.population.population}</p>
-              <p>{country.cca3.cca3}</p>
-              <p>
-                Area: {country.area.area}{" "}
-                {country.area.area > 100 ? "Big" : "Small"}
-              </p>
-              <button onClick={clickHandle} className="vbtn">
-                Not Visited
-              </button>
-            </div>
-          );
-        })}
+      <div className={`countrystyle ${visited && 'visited'}`}>
+        <h1>{country.name.common}</h1>
+        <img src={country.flags.flags.png} alt="" />
+        <p>Region: {country.region.region} </p>
+        <p>Population: {country.population.population}</p>
+        <div className="bottom">
+          <button className="btn" onClick={visitHandler}>{visited? 'Visited' : 'Not Visited'}</button>
+          <p className="fontxl">{ visited && '✅' }</p>
+        </div>
       </div>
     </>
   );
