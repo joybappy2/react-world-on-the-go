1ee90ce36a9725db3b7409be222c57eb8755c89e
joybappy2 <fan328547@gmail.com>
2025-09-19 20:04:51 +0600

    conditional rendering

--- world-on-the-go/src/components/Country/Country.jsx
+++ world-on-the-go/src/components/Country/Country.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import "./Country.css";
 
-const Country = ({ countries }) => {
+const Country = ( { countries } ) => {
+  const clickHandle = () => {
+    const btns = document.querySelectorAll( '.vbtn' )
+    btns.forEach( btn => {
+      
+    })
+  }
   return (
     <>
-      {countries.map((country) => {
-        console.log(country.flags.flags.png);
-        return (
-          <div key={country.cca3.cca3}>
-            <h3>Name: {country.name.common}</h3>
-            <img src={country.flags.flags.png} alt="" />
-          </div>
-        );
-      })}
+      <h1 className="title">Countries</h1>
+      <div className="country-container">
+        {countries.map((country) => {
+          return (
+            <div className="countrystyle" key={country.cca3.cca3}>
+              <img src={country.flags.flags.png} alt="" />
+              <h3>Name: {country.name.common}</h3>
+              <p>Population: {country.population.population}</p>
+              <p>{ country.cca3.cca3 }</p>
+              <p>
+                Area: { country.area.area } { country.area.area > 100 ? "Big" : "Small" }
+              </p>
+              <button onClick={clickHandle} className="vbtn">Not Visited</button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
