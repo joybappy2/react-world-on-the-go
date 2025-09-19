ec8bb5bd2061b61860ccad6b760ebc3cb9b6730d
joybappy2 <fan328547@gmail.com>
2025-09-19 09:49:49 +0600

    react world on the go project setup

---
+++ world-on-the-go/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Countries from "./components/Countries/Countries";
+
+function App() {
+  return (
+    <>
+      <h2>On the Go</h2>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
---
+++ world-on-the-go/src/components/Countries/Countries.jsx
@@ -0,0 +1,5 @@
+const Countries = () => {
+  return <div>I am from Countries component</div>;
+};
+
+export default Countries;
